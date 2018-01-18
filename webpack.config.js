3f35b81beaf9b022fce020e7fc03e15ac82664a9
Olivier Mobili <olivier.mobili@rtlnet.fr>
2018-01-18 15:50:50 +0100

    add webpack and typescript

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+const HOST = '127.0.0.1';
+const PORT = '8888';
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://'+HOST+':'+PORT,
+        './app/main.ts'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: "bundle.js",
+        pathinfo: true
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader']/*,
+                exclude: /node_modules|athena\.app/*/
+            },
+            {
+                test: /athena\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            }
+        ]
+    },
+    devServer: {
+        host: HOST,
+        port: PORT,
+
+        contentBase: './build/',
+
+        // inline hot-reload
+        inline: true
+    },
+    resolve: {
+        modules: [
+            'node_modules'
+        ]
+
+    },
+    plugins: [
+        new WebpackNotifierPlugin({
+            alwaysNotify: true,
+            skipFirstNotification: true,
+            title: 'AthenaJS-Breakout'
+        })
+    ]
+};
